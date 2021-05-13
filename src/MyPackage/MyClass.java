6caff91f82a3f729c4d2ef28cf3539e2705ac29a
sayali.gogawale <gogawale.sayli@gmail.com>
2021-05-14 01:32:50 +0530

    test

---
+++ src/MyPackage/MyClass.java
@@ -0,0 +1,33 @@
+package MyPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyClass {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\sayali.gogawale\\Downloads\\chromedriver.exe");
+
+            WebDriver driver = new ChromeDriver();
+            driver.navigate().to("https://www.google.com/");
+            driver.manage().window().maximize();
+            driver.findElement(By.name("q")).sendKeys("javatpoint tutorials");
+            driver.findElement(By.name("q")).getText();
+            Thread.sleep(10000);
+            driver.findElement(By.name("q")).clear();
+            Thread.sleep(10000);
+            driver.close();
+
+
+
+
+
+
+
+
+        }
+
+    }
